83b31df22ba7248c5588166895b9b20c4e77f237
Marc Jersey Castro <marcjerseycastro@gmail.com>
2025-05-13 23:50:05 +0800

    lexers. run bolang cli, variables palang
    YOOOOOO CONVERT TO SHREK NA THIS

---
+++ BoLang/BoLang/Lexer.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bolang
+{
+	public enum TokenType
+	{
+		Number,
+		Identifier,
+		Let,
+		BinaryOperator,
+		Equals,
+		OpenParen,
+		CloseParen
+	}
+
+	public class Token
+	{
+		public string Value { get; set; }
+		public TokenType Type { get; set; }
+
+		public Token(string value, TokenType type)
+		{
+			Value = value;
+			Type = type;
+		}
+
+		public override string ToString()
+		{
+			return $"value: \"{Value}\", type: {Type.ToString().ToLower()}";
+		}
+	}
+
+	public static class Lexer
+	{
+		private static readonly Dictionary<string, TokenType> KEYWORDS = new()
+		{
+			{ "let", TokenType.Let }
+		};
+
+		private static Token CreateToken(string value, TokenType type)
+		{
+			return new Token(value, type);
+		}
+
+		public static List<Token> Tokenize(string sourceCode)
+		{
+			List<Token> tokens = new();
+			List<char> src = new(sourceCode);
+
+			while (src.Count > 0)
+			{
+				char current = src[0];
+
+				if (current == '(')
+				{
+					tokens.Add(CreateToken(current.ToString(), TokenType.OpenParen));
+					src.RemoveAt(0);
+				}
+				else if (current == ')')
+				{
+					tokens.Add(CreateToken(current.ToString(), TokenType.CloseParen));
+					src.RemoveAt(0);
+				}
+				else if ("+-*/".Contains(current))
+				{
+					tokens.Add(CreateToken(current.ToString(), TokenType.BinaryOperator));
+					src.RemoveAt(0);
+				}
+				else if (current == '=')
+				{
+					tokens.Add(CreateToken(current.ToString(), TokenType.Equals));
+					src.RemoveAt(0);
+				}
+				else if (IsInt(current))
+				{
+					string num = "";
+					while (src.Count > 0 && IsInt(src[0]))
+					{
+						num += src[0];
+						src.RemoveAt(0);
+					}
+					tokens.Add(CreateToken(num, TokenType.Number));
+				}
+				else if (IsAlpha(current))
+				{
+					string ident = "";
+					while (src.Count > 0 && IsAlpha(src[0]))
+					{
+						ident += src[0];
+						src.RemoveAt(0);
+					}
+
+					if (KEYWORDS.ContainsKey(ident))
+					{
+						tokens.Add(CreateToken(ident, KEYWORDS[ident]));
+					}
+					else
+					{
+						tokens.Add(CreateToken(ident, TokenType.Identifier));
+					}
+				}
+				else if (IsSkippable(current))
+				{
+					src.RemoveAt(0);
+				}
+				else
+				{
+					Console.WriteLine($"Unrecognized character found in source: {(int)current} '{current}'");
+					Environment.Exit(1);
+				}
+			}
+
+			return tokens;
+		}
+
+		private static bool IsAlpha(char c) => char.IsLetter(c);
+		private static bool IsInt(char c) => char.IsDigit(c);
+		private static bool IsSkippable(char c) => char.IsWhiteSpace(c);
+	}
+}
---
+++ BoLang/BoLang/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Bolang;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Type 'run bolang' to run the system");
+		string input = Console.ReadLine()?.Trim().ToLower();
+
+		if (input == "run bolang")
+		{
+			string filePath = "C:\\xampp\\htdocs\\Projects\\BoLang\\BoLang\\BoLang\\code.txt"; // fix later and make it use relative path instead
+
+			if (File.Exists(filePath))
+			{
+				string code = File.ReadAllText(filePath);
+				var tokens = Lexer.Tokenize(code);
+
+				foreach (var token in tokens)
+				{
+					Console.WriteLine(token);
+				}
+			}
+			else
+			{
+				Console.WriteLine("Error: code.txt not found.");
+			}
+		}
+		else
+		{
+			Console.WriteLine("Command not recognized. Exiting...");
+		}
+	}
+}
